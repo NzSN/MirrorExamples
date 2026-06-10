3c023c7368778f4deff21e1723c7269bac4067a3
NzSN <nzsn0101@gmail.com>
2026-06-10 11:57:29 +0800

    Update Driver.

--- src/driver.ts
+++ src/driver.ts
@@ -2,25 +2,21 @@ import type { State } from "mirrorecma";
 
 export interface Drivable {
   toState(): State;
+  init(params: State): State;
   step(action: string, params: State): void;
 }
 
-export abstract class StateDriver {
-  abstract compute(action: string, params: State, prevState: State): State;
-}
-
-export class Driver<T extends Drivable> extends StateDriver {
+export class Driver<T extends Drivable> {
   private instance: T;
 
   constructor(private factory: () => T) {
-    super();
     this.instance = factory();
   }
 
   compute(action: string, params: State, prevState: State): State {
     if (action === "init" || action === "Init") {
       this.instance = this.factory();
-      return this.instance.toState();
+      return this.instance.init(params);
     }
     this.instance.step(action, params);
     return this.instance.toState();
